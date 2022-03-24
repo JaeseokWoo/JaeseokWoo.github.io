c6fa8b656c3d4e82dbe88c609f227073b6db35b1
JaeseokWoo <JaeseokW@gmail.com>
2022-03-25 00:18:46 +0900

    FEAT: 메인 페이지에서 Props로 받아 포스트 데이터 출력

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,15 +1,10 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import PostItem from 'components/Main/PostItem';
+import { PostListItemType } from 'types/PostItem.types';
 
-const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2020.01.29.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail: 'https://source.unsplash.com/random',
-  link: 'https://www.google.co.kr/',
+type PostListProps = {
+  posts: PostListItemType[];
 };
 
 const PostListWrapper = styled.div`
@@ -27,13 +22,12 @@ const PostListWrapper = styled.div`
   }
 `;
 
-const PostList: FunctionComponent = function () {
+const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
   return (
     <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
+      {posts.map(({ node: { id, frontmatter } }: PostListItemType) => (
+        <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
+      ))}
     </PostListWrapper>
   );
 };
