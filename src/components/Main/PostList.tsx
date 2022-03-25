5db0d94ada6098192ed00e8c8743eadcf184bbb2
JaeseokWoo <JaeseokW@gmail.com>
2022-03-25 17:01:33 +0900

    FEAT: 인피니티 스크롤 기능 구현

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent, useMemo } from 'react';
 import styled from '@emotion/styled';
 import PostItem from 'components/Main/PostItem';
 import { PostListItemType } from 'types/PostItem.types';
+import useInfiniteScroll, {
+  useInfiniteScroollType,
+} from 'hooks/useInfiniteScroll';
 
 type PostListProps = {
   selectedCategory: string;
@@ -27,24 +30,11 @@ const PostList: FunctionComponent<PostListProps> = function ({
   selectedCategory,
   posts,
 }) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostListItemType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
-  );
+  const { containerRef, postList } = useInfiniteScroll(selectedCategory, posts);
 
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostListItemType) => (
+    <PostListWrapper ref={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostListItemType) => (
         <PostItem {...frontmatter} link="https://www.google.co.kr/" key={id} />
       ))}
     </PostListWrapper>
