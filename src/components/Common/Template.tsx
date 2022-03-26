3ad8bb144ec609ff18c2d65913f378b38cf6a111
JaeseokWoo <JaeseokW@gmail.com>
2022-03-26 20:15:32 +0900

    FEAT: Semantic Tag 추가 및 기본 언어 설정하기

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -12,10 +12,10 @@ type TemplateProps = {
   children: ReactNode;
 };
 
-const Container = styled.div`
+const Container = styled.main`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100%;
 `;
 
 const Template: FunctionComponent<TemplateProps> = function ({
@@ -47,6 +47,8 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="twitter:image" content={image} />
         <meta name="twitter:site" content="@JaeseokWoo" />
         <meta name="twitter:creator" content="@JaeseokWoo" />
+
+        <html lang="ko" />
       </Helmet>
 
       <GlobalStyle />
